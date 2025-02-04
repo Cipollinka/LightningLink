9146bb41616c1d431c2b3d3a6bf2fab3db75db70
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-04 19:34:37 +0200

    Update Params.js

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = '690845c8-4ee1-4e62-84dd-0eab7cd49b5d'; // ключ OneSignal
   static appID = '6739270651'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
-  static targetDate = '2025-02-07'; // дата закінчення відльожки
+  static targetDate = '2025-02-03'; // дата закінчення відльожки
   static bodyLin = 'https://breathtaking-mega-satisfaction.space/13rQZMGm'; // тіло лінки з якою будемо працювати
 }
