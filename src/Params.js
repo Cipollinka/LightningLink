deb11a7c35c829fc0deab46df7e2c90f9e884112
o.podilko <opodilko122@gmail.com>
2025-02-04 17:36:11 +0200

    fix webview

--- src/Params.js
+++ src/Params.js
@@ -1,7 +1,7 @@
 export default class Params {
   static keyPush = '690845c8-4ee1-4e62-84dd-0eab7cd49b5d'; // ключ OneSignal
-  static appID = '6738986466'; // ID додатку в сторі
+  static appID = '6739270651'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
   static targetDate = '2025-02-07'; // дата закінчення відльожки
-  static bodyLin = 'https://amazing-cool-happiness.space/8CXm2XPN'; // тіло лінки з якою будемо працювати
+  static bodyLin = 'https://breathtaking-mega-satisfaction.space/13rQZMGm'; // тіло лінки з якою будемо працювати
 }
